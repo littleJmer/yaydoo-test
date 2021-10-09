5cc16baa56e0c451f38bca86e6565ae43a421e19
Manuel Escobedo <juanmanueler@gmail.com>
2021-10-09 15:30:32 -0700

    Test Cases / nyc dependency added
    It was added nyc dependency to display coverage report, also was
    modified test script to execute just files with prefix ".test.js" on
    their name.
    
    Currently "npm run test" executes a test case just as exampple.

---
+++ spec/after-30-days.test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+describe("Successful Cases", function () {
+
+    describe("Sum 1+1", function () {
+        it("should return 2", function () {
+            expect(1 + 1).to.be.equal(2);
+        });
+    });
+
+});
